fd11debd150db109996f2e85175f3ac2e702fd67
Unknown <zhangbolily@gmail.com>
2019-05-02 00:17:31 +0800

    Add Chinese locale translation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
+locale_dirs = ['locale/']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
